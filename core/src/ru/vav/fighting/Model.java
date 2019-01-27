51f552c55015b504f2a4d509b2d073414462dea4
NyanCoder-1 <vityaniko@mail.ru>
2019-01-27 11:39:22 +0900

    Commit reverted

--- core/src/ru/vav/fighting/Model.java
+++ core/src/ru/vav/fighting/Model.java
@@ -2,13 +2,14 @@ package ru.vav.fighting;
 
 public class Model {
 
+    // Singleton
     private static Model inst = new Model();
+    public static Model getInst() {
+        return inst;
+    }
 
     public void SendMessage(int msgID, int Message) {
 
     }
 
-    public static Model getInst() {
-        return inst;
-    }
 }
